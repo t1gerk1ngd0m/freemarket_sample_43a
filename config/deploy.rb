ce7a89b2981ea2cdfa34d5ebff4a5ba9a2e4960e
ryoutaku <uoyrutakoys@gmail.com>
2019-01-27 10:14:05 +0900

    Edit deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,3 +37,27 @@ set :repo_url, "git@example.com:me/my_repo.git"
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+# config valid only for current version of Capistrano
+lock '3.11.0'
+
+set :application, 'freemarket_sample_43a'
+set :repo_url,  'git@github.com:ryoutaku/freemarket_sample_43a.git'
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.3.1'
+
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['~/.ssh/free_market.pem']
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
