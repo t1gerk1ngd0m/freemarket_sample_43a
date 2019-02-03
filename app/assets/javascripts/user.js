798660e24ed2f7159295b5bce2c89dcd71c38a08
ryoutaku <uoyrutakoys@gmail.com>
2019-02-03 21:51:23 +0900

    Add Member Information Validation

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,13 +1,67 @@
-$(function() {
+$(function(){
+  var not_nill = new RegExp('\\S');
+  var mail_address = new RegExp("([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)");
+  var alphanumeric_6characters = new RegExp('[a-zA-Z0-9]{6,}');
+  var kana = new RegExp("");
+  var number = new RegExp('[0-9]');
+  var number_3or4_characters = new RegExp('[0-9]{3,4}');
   $('.signup-main__botton').on('click',function(){
     $('.signup-container__user').addClass('active');
     $('.signup-container__user').addClass('active');
     $('.signup-container__main').removeClass('active');
   });
-  $('.signup_button--user').on('click',function(){
+  $('.signup_button--user').on('click',function(e){
+    e.preventDefault();
+    var nickname = document.getElementById('user_nickname').value;
+    var email = document.getElementById('user_email').value;
+    var password = document.getElementById('user_password').value;
+    var password_confirmation = document.getElementById('user_password_confirmation').value;
+    var recaptcha = grecaptcha.getResponse();
+    if( not_nill.test(nickname) ){
+      $('.error--nickname').removeClass('active');
+    }else{
+      $('.error--nickname').addClass('active');
+    }
+    if( not_nill.test(email) ){
+      $('.error--email-nil').removeClass('active');
+    }else{
+      $('.error--email-nil').addClass('active');
+    }
+    if( mail_address.test(email) ){
+      $('.error--email-address').removeClass('active');
+    }else{
+      $('.error--email-address').addClass('active');
+    }
+    if( not_nill.test(password) ){
+      $('.error--password-nil').removeClass('active');
+    }else{
+      $('.error--password-nil').addClass('active');
+    }
+    if( alphanumeric_6characters.test(password) ){
+      $('.error--password-count').removeClass('active');
+    }else{
+      $('.error--password-count').addClass('active');
+    }
+    if( not_nill.test(password_confirmation) ){
+      $('.error--password_confirmation-nil').removeClass('active');
+    }else{
+      $('.error--password_confirmation-nil').addClass('active');
+    }
+    if( password == password_confirmation ){
+      $('.error--password_confirmation-match').removeClass('active');
+    }else{
+      $('.error--password_confirmation-match').addClass('active');
+    }
+    if( recaptcha ){
+      $('.error--recaptcha').removeClass('active');
+    }else{
+      $('.error--recaptcha').addClass('active');
+    }
+    if( not_nill.test(nickname) && not_nill.test(email) && mail_address.test(email) && not_nill.test(password) && alphanumeric_6characters.test(password) && not_nill.test(password_confirmation) && password == password_confirmation && recaptcha ){
     $('.input-field__tel').addClass('active');
     $('.progress__user').addClass('progress--done');
     $('.input-field__main').removeClass('active');
+    }
   });
   $('.signup_button--tel').on('click',function(){
     $('.input-field__address').addClass('active');
@@ -20,8 +74,3 @@ $(function() {
     $('.input-field__address').removeClass('active');
   });
 });
-
-
-
-
-
