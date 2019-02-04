a7a6a2830bd25706233f8ddb46b430191d22e08c
ryoutaku <uoyrutakoys@gmail.com>
2019-02-04 21:15:31 +0900

    Edit test name user.js

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -2,7 +2,7 @@ $(function(){
   var reg_not_nil = new RegExp('\\S');
   var reg_mail_address = new RegExp("([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)");
   var reg_alphanumeric_6characters = new RegExp('[a-zA-Z0-9]{6,}');
-  var reg_prefecture_choice = new RegExp("^(?!.*--未選択--).*$");
+  var reg_prefecture_choce = new RegExp("^(?!.*--未選択--).*$");
   var reg_only_kana = new RegExp("[ ァ-ヺ ]");
   var reg_intger_7_characters = new RegExp('[0-9]{7}');
   var reg_intger_10or11_characters = new RegExp('[0-9]{10,11}');
@@ -30,38 +30,54 @@ $(function(){
     var test_all_member_information = ( test_nil_nickname && test_nil_email && test_fomart_email && test_nil_password && test_fomart_password && test_nil_password_confirmation && test_match_password_confirmation && test_checked_recaptcha );
     if( test_nil_nickname ){
       $('.error--nickname').removeClass('active');
+      $('#user_nickname').addClass('signup_input');
     }else{
       $('.error--nickname').addClass('active');
+      $('#user_nickname').removeClass('signup_input');
     };
     if( test_nil_email  ){
       $('.error--email-nil').removeClass('active');
+      $('#user_email').addClass('signup_input');
     }else{
       $('.error--email-nil').addClass('active');
+      $('#user_email').removeClass('signup_input');
     };
     if( test_fomart_email ){
       $('.error--email-fomart').removeClass('active');
+      $('#user_email').addClass('signup_input');
     }else{
       $('.error--email-fomart').addClass('active');
-    };
-    if( test_nil_password ){
-      $('.error--password-nil').removeClass('active');
-    }else{
-      $('.error--password-nil').addClass('active');
-    };
-    if( test_fomart_password ){
-      $('.error--password-fomart').removeClass('active');
-    }else{
-      $('.error--password-fomart').addClass('active');
+      $('#user_email').removeClass('signup_input');
     };
     if( test_nil_password_confirmation ){
       $('.error--password_confirmation-nil').removeClass('active');
+      $('#user_password_confirmation').addClass('signup_input');
     }else{
       $('.error--password_confirmation-nil').addClass('active');
+      $('#user_password_confirmation').removeClass('signup_input');
     };
     if( test_match_password_confirmation ){
       $('.error--password_confirmation-match').removeClass('active');
+      $('#user_password_confirmation').addClass('signup_input');
     }else{
       $('.error--password_confirmation-match').addClass('active');
+      $('#user_password_confirmation').removeClass('signup_input');
+    };
+    if( test_nil_password ){
+      $('.error--password-nil').removeClass('active');
+      $('#user_password').addClass('signup_input');
+    }else{
+      $('.error--password-nil').addClass('active');
+      $('#user_password').removeClass('signup_input');
+      $('#user_password_confirmation').removeClass('signup_input');
+    };
+    if( test_fomart_password ){
+      $('.error--password-fomart').removeClass('active');
+      $('#user_password').addClass('signup_input');
+    }else{
+      $('.error--password-fomart').addClass('active');
+      $('#user_password').removeClass('signup_input');
+      $('#user_password_confirmation').removeClass('signup_input');
     };
     if( test_checked_recaptcha ){
       $('.error--recaptcha').removeClass('active');
@@ -69,9 +85,9 @@ $(function(){
       $('.error--recaptcha').addClass('active');
     };
     if( test_all_member_information ){
-    $('.input-field__tel').addClass('active');
-    $('.progress__user').addClass('progress--done');
-    $('.input-field__main').removeClass('active');
+      $('.input-field__tel').addClass('active');
+      $('.progress__user').addClass('progress--done');
+      $('.input-field__main').removeClass('active');
     };
   });
   $('.signup_button--tel').on('click',function(e){
@@ -82,8 +98,10 @@ $(function(){
     var test_all_phone_number_authentication = ( test_nil_tel_confirmation && test_fomart_tel_confirmation );
     if( test_nil_tel_confirmation ){
       $('.error--tel_confirmation-nil').removeClass('active');
+      $('#user_tel_confirmation').addClass('signup_input');
     }else{
       $('.error--tel_confirmation-nil').addClass('active');
+      $('#user_tel_confirmation').removeClass('signup_input');
     };
     if( test_fomart_tel_confirmation ){
       $('.input-field__address').addClass('active');
@@ -91,6 +109,7 @@ $(function(){
       $('.input-field__tel').removeClass('active');
     }else{
       $('.error--tel_confirmation-fomart').addClass('active');
+      $('#user_tel_confirmation').removeClass('signup_input');
     };
   });
   $('.signup_button--address').on('click',function(e){
@@ -105,63 +124,80 @@ $(function(){
     var block = document.getElementById('user_block').value;
     var test_nil_first_name = reg_not_nil.test(first_name);
     var test_nil_last_name = reg_not_nil.test(last_name);
-    var test_nil_first_name_kana = reg_only_kana.test(first_name_kana);
-    var test_nil_last_name_kana = reg_only_kana.test(last_name_kana);
-    var test_nil_zip = reg_intger_7_characters.test(zip);
-    var test_nil_prefecture = reg_prefecture_choice.test(prefecture);
+    var test_fomart_first_name_kana = reg_only_kana.test(first_name_kana);
+    var test_fomart_last_name_kana = reg_only_kana.test(last_name_kana);
+    var test_fomart_zip = reg_intger_7_characters.test(zip);
+    var test_choce_prefecture = reg_prefecture_choce.test(prefecture);
     var test_nil_city = reg_not_nil.test(city);
     var test_nil_block = reg_not_nil.test(block);
-    var test_nil_all_address = ( test_nil_first_name && test_nil_last_name && test_nil_first_name_kana && test_nil_last_name_kana && test_nil_zip && test_nil_prefecture && test_nil_city && test_nil_block );
+    var test_all_address = ( test_nil_first_name && test_nil_last_name && test_fomart_first_name_kana && test_fomart_last_name_kana && test_fomart_zip && test_choce_prefecture && test_nil_city && test_nil_block );
     if( test_nil_first_name ){
       $('.error--first_name').removeClass('active');
+      $('#user_first_name').addClass('signup_input');
     }else{
       $('.error--first_name').addClass('active');
+      $('#user_first_name').removeClass('signup_input');
     };
     if( test_nil_last_name ){
       $('.error--last_name').removeClass('active');
+      $('#user_last_name').addClass('signup_input');
     }else{
       $('.error--last_name').addClass('active');
+      $('#user_last_name').removeClass('signup_input');
     };
-    if( test_nil_first_name_kana ){
+    if( test_fomart_first_name_kana ){
       $('.error--first_name_kana').removeClass('active');
+      $('#user_first_name_kana').addClass('signup_input');
     }else{
       $('.error--first_name_kana').addClass('active');
+      $('#user_first_name_kana').removeClass('signup_input');
     };
-    if( test_nil_last_name_kana ){
+    if( test_fomart_last_name_kana ){
       $('.error--last_name_kana').removeClass('active');
+      $('#user_last_name_kana').addClass('signup_input');
     }else{
       $('.error--last_name_kana').addClass('active');
+      $('#user_last_name_kana').removeClass('signup_input');
     };
-    if( test_nil_zip ){
+    if( test_fomart_zip ){
       $('.error--zip').removeClass('active');
+      $('#user_zip').addClass('signup_input');
     }else{
       $('.error--zip').addClass('active');
+      $('#user_zip').removeClass('signup_input');
     };
-    if( test_nil_prefecture ){
+    if( test_choce_prefecture ){
       $('.error--prefecture').removeClass('active');
+      $('#user_prefecture').addClass('signup_input signup_input--prefecture');
     }else{
       $('.error--prefecture').addClass('active');
+      $('#user_prefecture').removeClass('signup_input signup_input--prefecture');
     };
     if( test_nil_city ){
       $('.error--city').removeClass('active');
+      $('#user_city').addClass('signup_input');
     }else{
       $('.error--city').addClass('active');
+      $('#user_city').removeClass('signup_input');
     };
     if( test_nil_block ){
       $('.error--block').removeClass('active');
+      $('#user_block').addClass('signup_input');
     }else{
       $('.error--block').addClass('active');
+      $('#user_block').removeClass('signup_input');
     };
-    if ( test_nil_all_address ){
+    if ( test_all_address ){
       $('.input-field__peymethod').addClass('active');
       $('.progress__address').addClass('progress--done');
       $('.input-field__address').removeClass('active');
     };
-  $('.signup_button--card').submit(function(e){
+  });
+  $('#new_user').submit(function(e){
     e.preventDefault(e);
     var card_number = document.getElementById('user_card_number').value;
-    var expiration_month = document.getElementById('user_expiration_month').value;
-    var expiration_year = document.getElementById('user_expiration_year').value;
+    var expiration_month = document.getElementById('user_expiration_month_2i').value;
+    var expiration_year = document.getElementById('expiration_year_date_1i').value;
     var security_code = document.getElementById('user_security_code').value;
     var test_nil_card_number = reg_not_nil.test(card_number);
     var test_fomart_card_number = reg_credit_card_number.test(card_number);
@@ -169,32 +205,53 @@ $(function(){
     var test_nil_expiration_year = reg_not_nil.test(expiration_year);
     var test_nil_security_code = reg_not_nil.test(security_code);
     var test_fomart_security_code = reg_intger_3or4_characters.test(security_code);
-    var test_nil_all_address = ( test_nil_card_number && test_fomart_card_number && test_nil_expiration_month && test_nil_expiration_year && test_nil_security_code && test_fomart_security_code );
+    var test_all_address = ( test_nil_card_number && test_fomart_card_number && test_nil_expiration_month && test_nil_expiration_year && test_nil_security_code && test_fomart_security_code );
     if( test_nil_card_number ){
-      $('.error--card_number').removeClass('active');
+      $('.error--card_number-nil').removeClass('active');
+      $('#user_nickname').addClass('signup_input');
     }else{
-      $('.error--card_number').addClass('active');
+      $('.error--card_number-mil').addClass('active');
+      $('#user_card_number').removeClass('signup_input');
+    };
+    if( test_fomart_card_number ){
+      $('.error--card_number-fomart').removeClass('active');
+      $('#user_card_number').addClass('signup_input');
+    }else{
+      $('.error--card_number-fomart').addClass('active');
+      $('#user_card_number').removeClass('signup_input');
     };
     if( test_nil_expiration_month ){
       $('.error--expiration_month').removeClass('active');
+      $('#user_expiration_month').addClass('input-form__birth');
     }else{
       $('.error--expiration_month').addClass('active');
+      $('#user_expiration_month').removeClass('input-form__birth');
     };
     if( test_nil_expiration_year ){
       $('.error--expiration_year').removeClass('active');
+      $('#user_expiration_year').addClass('input-form__birth');
     }else{
       $('.error--expiration_year').addClass('active');
+      $('#user_expiration_year').removeClass('input-form__birth');
     };
     if( test_nil_security_code ){
-      $('.error--security_code').removeClass('active');
+      $('.error--security_code-nil').removeClass('active');
+      $('#user_security_code').addClass('signup_input');
     }else{
-      $('.error--security_code').addClass('active');
+      $('.error--security_code-nil').addClass('active');
+      $('#user_security_code').removeClass('signup_input');
     };
-    if( test_nil_all_address ){
+    if( test_fomart_security_code ){
+      $('.error--security_code-fomart').removeClass('active');
+      $('#user_security_code').addClass('signup_input');
+    }else{
+      $('.error--security_code-fomart').addClass('active');
+      $('#user_security_code').removeClass('signup_input');
+    };
+    if( test_all_address ){
       this.submit();
     }else{
       return false;
     };
-    });
   });
 });
