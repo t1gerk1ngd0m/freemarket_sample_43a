e2ea51a8cfbd7aed02223c5941cf90e2ce08da94
Taiga Miwa <t1gerk1ngd0m@yahoo.co.jp>
2019-02-05 19:33:05 +0900

    implicate sider

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-  has_many :item_images
+  has_many :item_images, :dependent => :destroy
   accepts_nested_attributes_for :item_images, allow_destroy: true
   # with_options presence: true do
   #   validates :name
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to:'products#index'
-  resources :products do
-    resources :item_images, only: [:create]
-  end
+  resources :products
 end
