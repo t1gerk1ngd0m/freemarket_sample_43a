96a662009eca226c28f0fddb4704f2a288b0a796
tanaka111 <tanaka.naoki7014@gmail.com>
2019-02-06 20:18:02 +0900

    Merge remote-tracking branch 'ryoutaku/master' into product-detail-server

--- app/controllers/product_controller.rb
+++
@@ -1,5 +0,0 @@
-class ProductController < ApplicationController
-
-  def new
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to:'product#index'
-  resources :product
+  root to:'products#index'
+  resources :products
 end
