df4b498f987f6c79e6eba1a93a8de0f7a81ece7c
tanaka111 <tanaka.naoki7014@gmail.com>
2019-02-06 20:03:19 +0900

    商品詳細　画像　遷移機能追加

---
+++ app/assets/javascripts/product-show.js
@@ -0,0 +1,7 @@
+ $(function(){
+  $(".product-content__detail__summary__image__sub1").hover(function(){
+       $('.product-content__detail__summary__image__main1').attr('src',$(this).attr('src'))
+  });
+});
+
+
