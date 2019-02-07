e692784971b73d0c3edce15c1319c776db8ca593
ryoutaku <uoyrutakoys@gmail.com>
2019-02-07 16:50:41 +0900

    Edit /routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ devise_for :users, controllers: { sessions: 'users/sessions' , registraions: 'users/registraions' }
+  resources :users do
+  end
   root to:'product#index'
 end
