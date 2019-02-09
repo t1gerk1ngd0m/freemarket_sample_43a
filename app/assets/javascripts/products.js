65ff5dfac9b72c2a9cb0e6e064f83b306347de20
Taiga Miwa <t1gerk1ngd0m@yahoo.co.jp>
2019-02-09 19:23:13 +0900

    comment out drag&drop

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -33,9 +33,40 @@ $(function(){
     }
   });
 
+  // function drop(e) {
+  //   e.preventDefault();
+  //   e.stopPropagation();
+  //   var file = e.dataTransfer.files[0];
+  //       reader = new FileReader(),
+  //       $preview = $(".image-upload-items ul");
+  //   console.log(file);
+  //   if(file.type.indexOf("image") < 0){
+  //     return false;
+  //   }
+
+  //   reader.onload = (function(file) {
+  //     return function(e) {
+  //       var loadedImageUri = e.target.result;
+  //       var appendImage = `<li class="image-upload-item">
+  //                           <figure class="sell-upload-figure portrait">
+  //                             <img src="${ loadedImageUri }" alt class>
+  //                           </figure>
+  //                           <div class="sell-upload-button"><!--
+  //                             --><a href class="sell-upload-edit">編集</a><!--
+  //                             --><a href>削除</a><!--
+  //                           --></div>
+  //                         </li>`
+  //       // .prevewの領域の中にロードした画像を表示するimageタグを追加
+  //       $preview.append(appendImage)
+  //     };
+  //   })(file);
+  //   reader.readAsDataURL(file);
+  // };
+
+
   // 画像アップロード時のview変更
   $('#product_item_images_attributes_0_name').change(function(e) {
-    var file = e.target.files[i],
+    var file = e.target.files[0],
         reader = new FileReader(),
         $preview = $(".image-upload-items ul");
     // 画像ファイル以外の場合は何もしない
@@ -56,9 +87,6 @@ $(function(){
                               --><a href>削除</a><!--
                             --></div>
                           </li>`
-        // console.log(e);
-        //既存のプレビューを削除
-        // $preview.empty();
         // .prevewの領域の中にロードした画像を表示するimageタグを追加
         $preview.append(appendImage)
       };
