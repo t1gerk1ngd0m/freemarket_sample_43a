a134b92f0ab5fb1b4da7e640c0a83dd8fe218817
ryoutaku <uoyrutakoys@gmail.com>
2019-02-10 16:51:02 +0900

    Add user "provider"  "uid"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190208030353) do
+ActiveRecord::Schema.define(version: 20190210074624) do
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       default: "", null: false
@@ -22,16 +22,6 @@ ActiveRecord::Schema.define(version: 20190208030353) do
     t.index ["lft"], name: "index_categories_on_lft", using: :btree
     t.index ["parent_id"], name: "index_categories_on_parent_id", using: :btree
     t.index ["rgt"], name: "index_categories_on_rgt", using: :btree
-  end
-
-  create_table "creditcards", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "user_id"
-    t.string   "card_number",     null: false
-    t.integer  "expiration_date", null: false
-    t.string   "security_code",   null: false
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.index ["user_id"], name: "index_creditcards_on_user_id", using: :btree
   end
 
   create_table "item_images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -68,49 +58,38 @@ ActiveRecord::Schema.define(version: 20190208030353) do
     t.datetime "updated_at",              null: false
   end
 
-  create_table "tells", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "tell_number", default: "", null: false
-    t.integer  "user_id"
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-    t.index ["user_id"], name: "index_tells_on_user_id", using: :btree
-  end
-
-  create_table "user_details", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "user_id"
-    t.string   "first_name",      null: false
-    t.string   "last_name",       null: false
-    t.string   "first_name_kana", null: false
-    t.string   "last_name_kana",  null: false
-    t.string   "zip",             null: false
-    t.string   "prefecture",      null: false
-    t.string   "city",            null: false
-    t.string   "block",           null: false
-    t.string   "building"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.index ["user_id"], name: "index_user_details_on_user_id", using: :btree
-  end
-
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "nickname",               default: "",           null: false
     t.string   "email",                  default: "",           null: false
     t.string   "encrypted_password",     default: "",           null: false
+    t.string   "tel_confirmation",                              null: false
+    t.string   "first_name",                                    null: false
+    t.string   "last_name",                                     null: false
+    t.string   "first_name_kana",                               null: false
+    t.string   "last_name_kana",                                null: false
+    t.string   "zip",                                           null: false
+    t.integer  "prefecture",             default: 0,            null: false
+    t.string   "city",                                          null: false
+    t.string   "block",                                         null: false
+    t.string   "building",               default: ""
+    t.string   "phone_number",           default: ""
+    t.string   "card_number",                                   null: false
+    t.date     "expiration_month",       default: '1900-01-01', null: false
+    t.date     "expiration_year",        default: '1900-01-01', null: false
+    t.string   "security_code",                                 null: false
     t.string   "profile",                default: ""
-    t.string   "payment_method",         default: ""
     t.date     "birthday",               default: '1900-01-01', null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at",                                    null: false
     t.datetime "updated_at",                                    null: false
-    t.string   "nickname",               default: "",           null: false
+    t.string   "provider"
+    t.string   "uid"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
-  add_foreign_key "creditcards", "users"
   add_foreign_key "item_images", "products"
   add_foreign_key "products", "users"
-  add_foreign_key "tells", "users"
-  add_foreign_key "user_details", "users"
 end
