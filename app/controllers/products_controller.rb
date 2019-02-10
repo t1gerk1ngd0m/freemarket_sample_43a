71f9d48d1b3a60e0c04184be73bc16f1531a5a80
Taiga Miwa <t1gerk1ngd0m@yahoo.co.jp>
2019-02-10 23:35:42 +0900

    make selection of categories&sizes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,20 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
     @item_image = @product.item_images.build
+
+    @category_larges = Category.roots
+    @category_middles = []
+    @category_smalls = []
+    for @category_large in @category_larges
+      @category_middle = @category_large.children
+      for @category_middle_small in @category_middle
+        @category_small = @category_middle_small.children
+        @category_smalls << @category_small
+      end
+      @category_middles << @category_middle
+    end
+
+    @sizes = Size.all
   end
 
   def create
@@ -36,6 +50,7 @@ class ProductsController < ApplicationController
       :shipping_method,
       :number_of_the_days_to_ship,
       :price, :condition,
-      item_images_attributes: [:name])
+      item_images_attributes: [:name]
+    )
   end
 end
