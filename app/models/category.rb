fbd33af6fa40110f289c2c554a8cfa80d11542b7
Taiga Miwa <t1gerk1ngd0m@yahoo.co.jp>
2019-02-10 23:34:36 +0900

    make association products with sizes&categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
   acts_as_nested_set
+  has_many :products
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,20 +1,24 @@
 class Product < ApplicationRecord
   has_many :item_images, :dependent => :destroy
   accepts_nested_attributes_for :item_images, allow_destroy: true
-  # with_options presence: true do
-  #   validates :name
-  #   validates :description
-  #   validates :category_large
-  #   validates :category_middle
-  #   validates :category_small
-  #   validates :size
-  #   validates :shipping_charges_burden
-  #   validates :dispatch_area
-  #   validates :shipping_method
-  #   validates :number_of_the_days_to_ship
-  #   validates :price
-  #   validates :condition
-  # end
+  belongs_to :category_large, class_name: "Category"
+  belongs_to :category_middle, class_name: "Category"
+  belongs_to :category_small, class_name: "Category"
+  belongs_to :size
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :category_large
+    validates :category_middle
+    validates :category_small
+    validates :size
+    validates :shipping_charges_burden
+    validates :dispatch_area
+    validates :shipping_method
+    validates :number_of_the_days_to_ship
+    validates :price
+    validates :condition
+  end
 
   enum dispatch_area: {
    "---": 0, 北海道: 1,青森県: 2,岩手県: 3,宮城県: 4,秋田県: 5,山形県: 6,福島県: 7,
@@ -25,5 +29,22 @@ class Product < ApplicationRecord
    鳥取県: 31, 島根県: 32, 岡山県: 33, 広島県: 34, 山口県: 35,
    徳島県: 36, 香川県: 37, 愛媛県: 38, 高知県: 39,
    福岡県: 40, 佐賀県: 41, 長崎県: 42, 熊本県: 43, 大分県: 44, 宮崎県: 45, 鹿児島県: 46, 沖縄県: 47, 未定: 99
-  }
+  }, _suffix: true
+
+  enum condition: {
+   "---": 0, "新品、未使用": 1, "未使用に近い": 2, "目立った傷や汚れなし": 3, "やや傷や汚れあり": 4, "傷や汚れあり": 5, "全体的に状態が悪い": 6
+  }, _suffix: true
+
+  enum shipping_charges_burden: {
+   "---": 0, "送料込み(出品者負担)": 1, "着払い(購入者負担)": 2
+  }, _suffix: true
+
+  enum shipping_method: {
+   "---": 0, "未定": 1, "らくらくメルカリ便": 2, "ゆうメール": 3, "レターパック": 4, "普通郵便(定形、定形外)": 5, "普通郵便(定形、定形外)": 6, "クロネコヤマト": 7, "ゆうパック": 8, "クリックポスト": 9, "ゆうパケット": 10
+  }, _suffix: true
+
+  enum number_of_the_days_to_ship: {
+   "---": 0, "1~2日で発送": 1, "2~3日で発送": 2, "4~7日で発送": 3
+  }, _suffix: true
+
 end
