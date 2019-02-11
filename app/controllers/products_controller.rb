928a79e6c4ab011760a08a40aad544437c4aa5b6
tanaka111 <tanaka.naoki7014@gmail.com>
2019-02-11 21:51:13 +0900

    商品詳細　サーバー　インスタンス追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,6 +16,8 @@ class ProductsController < ApplicationController
 
   def show
     @product =Product.find(params[:id])
+    @products =Product.includes(:item_images).limit(6)
+
   end
 
   private
