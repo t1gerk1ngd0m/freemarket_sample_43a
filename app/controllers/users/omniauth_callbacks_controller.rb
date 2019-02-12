697b855f281e4d5c16b675623a9eccfdbe6a8f35
ryoutaku <uoyrutakoys@gmail.com>
2019-02-12 16:14:28 +0900

    Edit omniauth_callbacks_controller.rb

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,18 +3,20 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     callback_from :facebook
   end
 
+  def google_oauth2
+    callback_from :google
+  end
+
   private
 
   def callback_from(provider)
     provider = provider.to_s
-
     @user = User.find_for_oauth(request.env['omniauth.auth'])
 
     if @user.persisted?
       flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
       sign_in_and_redirect @user, event: :authentication
     else
-      session["devise.#{provider}_data"] = request.env['omniauth.auth']
       session[:nickname] = @user.nickname
       session[:email] = @user.email
       session[:password] = @user.password
