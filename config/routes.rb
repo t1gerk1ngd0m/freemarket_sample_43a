b20f4677c7321d3cdbf856b39c8080f63a108e86
ryoutaku <uoyrutakoys@gmail.com>
2019-02-12 15:07:31 +0900

    Edit config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,21 @@ Rails.application.routes.draw do
  devise_for :users,
  controllers: {
   sessions: 'users/sessions' ,
-  registraions: 'users/registraions' ,
+  registrations: 'users/registrations' ,
   omniauth_callbacks: 'users/omniauth_callbacks'
   }
 
-  resources :users do
+  resources :users , :except => [:new, :create] do
     collection do
       get 'card', to: 'users#card', as: 'card'
       get 'logout', to: 'users#logout', as: 'logout'
       get 'identification', to: 'users#identification', as: 'identification'
       get 'profile', to: 'users#profile', as: 'profile'
     end
+  end
+
+  devise_scope :user do
+    get 'signup/sns', to: 'users/registrations#sns', as: 'new_user_registration_sns'
   end
 
   root to:'products#index'
