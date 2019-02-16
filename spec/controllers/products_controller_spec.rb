8096cdc8ff6651f68eb00d021c1039594edc5a25
Taiga Miwa <t1gerk1ngd0m@yahoo.co.jp>
2019-02-16 15:11:07 +0900

    write controller test of new&edit&preview

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -9,24 +9,57 @@ describe ProductsController do
   end
 
   describe "#show" do
-      it "assigns the requested product to @product" do
-        product = create(:product)
-        get :show, params: { id: product }
-        expect(assigns(:product)).to eq product
+    it "assigns the requested product to @product" do
+      product = create(:product)
+      get :show, params: { id: product }
+      expect(assigns(:product)).to eq product
     end
   end
   describe "GET #show" do
-     it "renders the :show template" do
-        product = create(:product)
-        get :show, params: { id: product }
-        expect(response).to render_template :show
-     end
+    it "renders the :show template" do
+      product = create(:product)
+      get :show, params: { id: product }
+      expect(response).to render_template :show
+    end
   end
   describe "POST #pay" do
-     it "renders the :show template" do
-        product = create(:product)
-        post :pay, params: { id: product }
-        expect(response).to render_template :show
-     end
+    it "renders the :show template" do
+      product = create(:product)
+      post :pay, params: { id: product }
+      expect(response).to render_template :show
+    end
+  end
+  describe "GET #new" do
+    it "renders the :new template" do
+      product = create(:product)
+      get :new
+      expect(response).to render_template :new
+    end
+  end
+
+  describe "GET #edit" do
+    it "assigns the requested product to @product" do
+      product = create(:product)
+      get :edit, params: { id: product }
+      expect(assigns(:product)).to eq product
+    end
+    it "renders the :edit template" do
+      product = create(:product)
+      get :edit, params: {id: product}
+      expect(response).to render_template :edit
+    end
+  end
+
+  describe "GET #preview" do
+    it "assigns the requested product to @product" do
+      product = create(:product)
+      get :preview, params: { id: product }
+      expect(assigns(:product)).to eq product
+    end
+    it "renders the :preview template" do
+      product = create(:product)
+      get :preview, params: {id: product}
+      expect(response).to render_template :preview
+    end
   end
 end
