368381b74aaf9756fda89ec90de81e4902ff8f86
Taiga Miwa <t1gerk1ngd0m@yahoo.co.jp>
2019-02-16 18:18:34 +0900

    write dependent option

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[facebook google_oauth2]
-  has_many :products
+  has_many :products, :dependent => :destroy
 
   def self.find_for_oauth(auth)
     user = User.where(uid: auth.uid, provider: auth.provider).first
