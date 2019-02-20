dd4ad5569aa2f9dd4839b31dfc03819b7cf77712
tanaka111 <tanaka.naoki7014@gmail.com>
2019-02-20 17:07:38 +0900

    likeコントローラー　追加

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,17 +4,19 @@ class LikesController < ApplicationController
   def like
     like = current_user.likes.new(product_id: @product.id)
     like.save
+    redirect_to :controller => 'products', :action => 'show'
   end
 
   def unlike
     like = current_user.likes.find_by(product_id: @product.id)
     like.destroy
+    redirect_to @product
   end
 
   private
 
   def set_variables
-    @product = Product.find(params[:product_id])
+    @product = Product.find(params[:id])
     @id_name = "#like-link-#{@product.id}"
   end
 
