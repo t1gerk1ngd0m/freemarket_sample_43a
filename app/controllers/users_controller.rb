9c8efdac28cb7004cd093ffdfe67f9b2b0efe567
ryoutaku <uoyrutakoys@gmail.com>
2019-02-20 15:57:13 +0900

    Add trading action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,8 @@ class UsersController < ApplicationController
     @products = Product.where(user_id: current_user , status: 0).includes(:item_images)
   end
 
+  def trading
+    @products = Product.where(user_id: current_user , status: 1).includes(:item_images)
+  end
+
 end
